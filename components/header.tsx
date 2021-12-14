7e744d2671d1cbedd6123af7f85c9d82175f4ccb
Andrew Osei-Wusu <andrewosei94@gmail.com>
2021-12-14 19:37:32 +0000

    UI changes to mobile header

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ const Header: FC<{ onGetQuote?: () => void }> = ({ onGetQuote }) => {
           <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
             <div className="flex flex-row items-center justify-start lg:w-0 lg:flex-1 space-x-10">
               <Link href="/" passHref>
-                <a>
+                <a href="#">
                   <img className="cursor-pointer w-auto" src="/img/logo.svg" alt="" />
                 </a>
               </Link>
@@ -77,14 +77,12 @@ const Header: FC<{ onGetQuote?: () => void }> = ({ onGetQuote }) => {
             focus
             className="z-50 absolute top-0 inset-x-0 transition transform origin-top-right md:hidden"
           >
-            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white space-y-4">
               <div className="pt-5 px-5">
                 <div className="flex items-center justify-between">
-                  <div>
-                    <Link href="/" passHref>
-                      <img className="h-20 w-auto sm:h-28" src="/img/logo.svg" alt="" />
-                    </Link>
-                  </div>
+                  <Link href="/" passHref>
+                    <img className="h-20 w-1/4 sm:h-28" src="/img/logo.svg" alt="" />
+                  </Link>
                   <div className="-mr-2">
                     <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-border">
                       <span className="sr-only">Close menu</span>
@@ -95,17 +93,17 @@ const Header: FC<{ onGetQuote?: () => void }> = ({ onGetQuote }) => {
                 <div className="mt-6">
                   <nav className="grid gap-y-8">
                     <Link href="/how-it-works">
-                      <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 ml-3 text-base font-medium text-gray-900">
+                      <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 text-base font-medium text-gray-900">
                         How it works
                       </span>
                     </Link>
                     <Link href="/legal?section=faq">
-                      <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 ml-3 text-base font-medium text-gray-900">
+                      <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 text-base font-medium text-gray-900">
                         FAQs
                       </span>
                     </Link>
                     <Link href="/network">
-                      <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 ml-3 text-base font-medium text-gray-900">
+                      <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 text-base font-medium text-gray-900">
                         Network
                       </span>
                     </Link>
