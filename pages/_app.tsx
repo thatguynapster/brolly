9a5f4a9715924d84f1d8d310071690ba075850df
Andrew Osei <andrewosei94@gmail.com>
2022-04-12 16:11:52 +0000

    Addition added gtag to project - all analytics only load in production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,7 @@ import "../public/css/carousel.css";
 import "react-toastify/dist/ReactToastify.css";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -44,6 +45,25 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+
+      {process.env.NEXT_PUBLIC_ENVIRONMENT === "production" && (
+        <>
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script strategy="lazyOnload">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+          </Script>
+        </>
+      )}
       <ToastContainer
         autoClose={2000}
         pauseOnFocusLoss={false}
